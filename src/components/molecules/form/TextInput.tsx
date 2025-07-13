ee47b9eda5c8945b6a6a5715b601651ce9f7aae1
b-zurg <buzurg.arjmandi@gmail.com>
2025-07-13 20:04:27 +0200

    chore: update Tailwind CSS and Vite dependencies

--- src/components/molecules/form/TextInput.tsx
+++ src/components/molecules/form/TextInput.tsx
@@ -16,8 +16,7 @@ export const TextInput: React.FC<TextInputProps> = ({
     <input
       type="text"
       placeholder={initialValue}
-      className="mt-1 text-gray-900 block w-full px-3 py-2 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
-    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+      className="mt-1 text-gray-900 block w-full px-3 py-2 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 bg-white focus:bg-white focus:text-gray-900 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
       onChange={(event) => debouncedChange(event.target.value)}
     />
   );
